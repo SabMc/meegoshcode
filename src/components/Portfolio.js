42ceb642e80567937216772681c4db6f5c228add
SabMc <74684143+SabMc@users.noreply.github.com>
2021-12-16 17:48:32 +0100

    Layout Portfolio
    New layout to display Portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Project from "./Project";
+//import Project from "./Project";
 import "./Portfolio.css";
+import Project2 from "./Project2";
 import dacfront from "./images/dacfront.jpg";
 import sailingapp from "./images/sailingapp.jpg";
 
@@ -11,28 +12,28 @@ function Portfolio() {
         <h2 className="article-title">Portfolio</h2>
     
           <div className="projects-container">
-            <Project
-              path="https://dutchartclass.nl"
-              img={dacfront}
-              titel="Dutch Art Class"
-              text="Creating an online art school. From the idea to the execution of
-              building a homepage, training the art teacher in giving online
-              sessions to developing a promotion plan"
-            />
-            <Project
-              path="https://dutchartclass.nl"
-              img={dacfront}
-              titel="Dutch Art Class"
-              text="Creating an online art school. From the idea to the execution of
-              building a homepage, training the art teacher in giving online
-              sessions to developing a promotion plan"
-            />
-            <Project
-            path="https://focused-stonebraker-0d6f32.netlify.app"
-            img={sailingapp}
-            titel="Sailing Weather App"
-            text="Creating a sailing app to check the weather. Calculating wind speed to beaufort at the current location and which ever location requiered"
+          <Project2 
+          img={dacfront}
+          alt="dutch art class frontpage pic"
+          description="Online Art School"
+          titel="Dutch Art Class"
+          text="Creating an online art school. From the idea to the execution of
+          building a homepage, training the art teacher in giving online
+          sessions to developing a promotion plan "
+          url="https://dutchartclass.nl"
+          
           />
+          
+          <Project2 
+          img={sailingapp}
+          alt="sailing app frontpage picture"
+          description="Final Project React Workshop"
+          titel="Sailing Weather App"
+          text="Creating a sailing app to check the weather. Calculating wind speed to Beaufort at the current location and which ever location requiered"
+          url="https://focused-stonebraker-0d6f32.netlify.app"
+          
+          />
+
           </div>
         
       </div>
---
+++ src/components/Project2.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function Project2 (props){return (
+<div className="grid">
+<figure class="effect-marley">
+    <img src={props.img} alt={props.alt}/>
+        <figcaption>
+              <h2>{props.description}<br/>
+              <span>{props.titel}</span>
+              </h2>
+              <p>{props.text}<br/>
+              <a href={props.url}>View more</a>
+              </p>
+              
+            </figcaption>     
+          </figure>
+    
+    </div>
+    );
+}
+
+export default Project2;
