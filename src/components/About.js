ab97297186c95554f8bc664ad7a8b7c8baec6f62
SabMc <74684143+SabMc@users.noreply.github.com>
2022-01-25 15:23:40 +0100

    adjust font-sizes
     to increase readability

--- src/components/About.js
+++ src/components/About.js
@@ -17,10 +17,14 @@ function About() {
           <div className="description two">
             <h2 className="article-title">Sabrina from Meegosh Code</h2>
             <p className="short-text">
-            Meegosh Code offers customised digital solutions for small businesses and artists. 
-            <ul className="service-list"><li>Creating and maintaining websites
-</li><li>Building online courses</li><li>Developing a marketing plan</li><li>Setting up virtual events</li></ul>
+            Meegosh Code offers customised digital solutions for small businesses and artists 
             </p>
+            <ul className="service-list">
+              <li>Creating and maintaining websites</li>
+              <li>Building online courses</li>
+              <li>Developing a marketing plan</li>
+              <li>Setting up virtual events</li></ul>
+           
           </div>
         </div>
       </div>
